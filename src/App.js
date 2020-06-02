7ec710e28a2e4e565cf9607ea1969aaa3d3c4aed
Tóth Máté <tothmate911@gmail.com>
2020-06-02 10:28:06 +0200

    craete navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 import './App.css';
+
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Reactomon</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <div className="container">
+          <Navbar />
+          <Route path="/pokemons" />
+          <Route path="/types" />
+        </div>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default function Navbar() {
+  return (
+    <div style={navbarStyle}>
+      <Link to="/">Home</Link>
+      {'  |  '}
+      <Link to="/pokemons">Pokemons</Link>
+      {'  |  '}
+      <Link to="/types">Types</Link>
+    </div>
+  );
+}
+
+const navbarStyle = {
+  background: '#555',
+  textAlign: 'center',
+  padding: '10px',
+  color: '#fff',
+};
